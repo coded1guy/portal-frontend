67b07fdeaa0f97c4da4757f780025196f402a917
coded1guy <grggmmnl@gmail.com>
2023-03-30 22:27:39 +0100

    refactor: added nextJS boilerplate

--- js/filterData.js
+++
@@ -1,6 +0,0 @@
-const formParent = document.querySelector("#formParent");
-console.log(formParent);
-const formChildren = formParent.querySelectorAll("input");
-console.log(formChildren);
-const [ country, state, city, jobId, level, status ] = formChildren;
-console.log(country);
